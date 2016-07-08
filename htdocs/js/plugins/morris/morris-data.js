4a88d8a9f14badd81c213d44b361df5671531995
Lucas Cantarelli <lgcantarelli@gmail.com>
2016-07-08 20:09:19 -0300

    estrutura inicial

--- htdocs/js/plugins/morris/morris-data.js
+++ htdocs/js/plugins/morris/morris-data.js
@@ -193,35 +193,33 @@ $(function() {
     });
 
     // Bar Chart
-    Morris.Bar({
-        element: 'morris-bar-chart',
-        data: [{
-            device: 'iPhone',
-            geekbench: 136
-        }, {
-            device: 'iPhone 3G',
-            geekbench: 137
-        }, {
-            device: 'iPhone 3GS',
-            geekbench: 275
-        }, {
-            device: 'iPhone 4',
-            geekbench: 380
-        }, {
-            device: 'iPhone 4S',
-            geekbench: 655
-        }, {
-            device: 'iPhone 5',
-            geekbench: 1571
-        }],
-        xkey: 'device',
-        ykeys: ['geekbench'],
-        labels: ['Geekbench'],
-        barRatio: 0.4,
-        xLabelAngle: 35,
-        hideHover: 'auto',
-        resize: true
-    });
-
-
+    // Morris.Bar({
+    //     element: 'morris-bar-chart',
+    //     data: [{
+    //         device: 'iPhone',
+    //         geekbench: 136
+    //     }, {
+    //         device: 'iPhone 3G',
+    //         geekbench: 137
+    //     }, {
+    //         device: 'iPhone 3GS',
+    //         geekbench: 275
+    //     }, {
+    //         device: 'iPhone 4',
+    //         geekbench: 380
+    //     }, {
+    //         device: 'iPhone 4S',
+    //         geekbench: 655
+    //     }, {
+    //         device: 'iPhone 5',
+    //         geekbench: 1571
+    //     }],
+    //     xkey: 'device',
+    //     ykeys: ['geekbench'],
+    //     labels: ['Geekbench'],
+    //     barRatio: 0.4,
+    //     xLabelAngle: 35,
+    //     hideHover: 'auto',
+    //     resize: true
+    // });
 });
